3d8a64a48d2f7a17dbab60b3276cdff185b14fe3
PatrikG <Patrik0Gn@gmail.com>
2021-08-02 09:58:02 +0200

    Add endpoints and modules for books and author

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const db = require("./lib/db");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Book = require("./models/book");
+const Author = require("./models/author");
 
 /*
   We create an express app calling
@@ -12,22 +15,131 @@ const app = express();
   - parse the body of the request to json for us
   https://expressjs.com/en/guide/using-middleware.html
 */
+app.use(cors());
 app.use(express.json());
+app.use(function log(req, res, next) {
+  req.time = new Date();
+  console.log(req.time, req.method, req.hostname + req.originalUrl);
+  next();
+});
 
 /*
   Endpoint to handle GET requests to the root URI "/"
 */
 app.get("/", (req, res) => {
   res.json({
-    "/articles": "read and create new articles",
-    "/articles/:id": "read, update and delete an individual article",
+    "/books": "list of books",
   });
+});
+
+app.get("/books", (req, res) => {
+  const genre = req.query.genre;
+  Book.find({ genre: `${genre}` })
+    .populate("author")
+    .then((books) => {
+      console.log(books);
+      res.status(200).send(books);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.get("/books/:id", function (req, res) {
+  Book.findById(req.params.id)
+    .then((book) => {
+      if (book) {
+        console.log("Book found");
+        res.send(book);
+      } else {
+        console.log("Book not found");
+        res.status(404).json("404 Book not found"); //.send only send json if object or array. If string than its plain text.
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.post("/books", (req, res) => {
+  Book.create(req.body)
+    .then((newBook) => {
+      res.status(201).send(newBook);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+
+app.patch("/books/:id", function (req, res) {
+  Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedBook) => {
+      if (updatedBook) {
+        console.log("Article updated");
+        res.status(200).send(updatedBook);
+      } else {
+        console.log("Article not found, hence couldn't be updated");
+        res.status(404).send("404 Article not found");
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.delete("/books/:id", function (req, res) {
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => {
+      console.log("deleted successfully");
+      res.status(204).end(); //just end without sending a body
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.get("/authors", (req, res) => {
+  Author.find()
+    .then((authors) => {
+      console.log(authors);
+      res.send(authors);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.post("/authors", (req, res) => {
+  Author.create(req.body)
+    .then((newAuthor) => {
+      res.status(201).send(newAuthor);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
 
 /*
   We have to start the server. We make it listen on the port 4000
 
 */
-app.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
-});
+mongoose
+  .connect("mongodb://localhost:27017/books-api", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to mongo");
+    app.listen(4000, () => {
+      console.log("Listening on http://localhost:4000");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
